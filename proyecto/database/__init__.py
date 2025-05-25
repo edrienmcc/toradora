666818668994f94c5581638c13e0cf819ae1a232
edrienmcc <nohemiazux@gmail.com>
2025-05-25 11:08:48 -0500

    Delete proyecto directory

--- proyecto/database/__init__.py
+++
@@ -1,5 +0,0 @@
-from .config import DatabaseConfig
-from .category_manager import CategoryManager
-from .wordpress_publisher import WordPressPublisher
-
-__all__ = ['DatabaseConfig', 'CategoryManager', 'WordPressPublisher']
--- proyecto/scheduler/__init__.py
+++
@@ -1,5 +0,0 @@
-# proyecto/scheduler/__init__.py
-from .task_scheduler import TaskScheduler, ScheduledTask
-from .auto_scraper import AutoScraper
-
-__all__ = ['TaskScheduler', 'ScheduledTask', 'AutoScraper']
